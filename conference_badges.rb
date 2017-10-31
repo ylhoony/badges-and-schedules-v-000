7e120b41ae417aad89fedbceb64f79b327ab91d2
Hoon Lee <yl.hoony@gmail.com>
2017-10-31 05:57:41 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,27 +4,37 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
+# def batch_badge_creator(attendees)
+#   result = []
+#   attendees.each do |e|
+#     result << badge_maker(e)
+#   end
+#   result
+# end
+
 def batch_badge_creator(attendees)
-  result = []
-  attendees.each do |e|
-    result << badge_maker(e)
+  attendees.map do |attendee|
+    "Hello, my name is #{attendee}."
   end
-  result
 end
 
+# def assign_rooms(attendees)
+#   result = []
+#   counter = 1
+#   attendees.each do |e|
+#     result << "Hello, #{e}! You'll be assigned to room #{counter}!"
+#     counter += 1
+#   end
+#   result
+# end
+
 def assign_rooms(attendees)
-  result = []
-  counter = 1
-  attendees.each do |e|
-    result << "Hello, #{e}! You'll be assigned to room #{counter}!"
-    counter += 1
+  attendees.each_with_index.map do |attendee, index|
+    "Hello, #{attendee}! You'll be assigned to room #{index+1}!"
   end
-  result
 end
 
 def printer(attendees)
-  a = batch_badge_creator(attendees)
-  a.each { |e| puts e }
-  b = assign_rooms(attendees)
-  b.each { |e| puts e}
+  batch_badge_creator(attendees).each { |e| puts e }
+  assign_rooms(attendees).each { |e| puts e}
 end
